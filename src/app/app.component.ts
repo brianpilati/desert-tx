b0f3a5b3ed3d9b785c0437825e1b95ca988bf240
Brian Pilati <brianpilati@gmail.com>
2018-08-10 21:54:22 -0600

    Started the html and lambda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desert-tx';
+
+  acceptedMimeTypes = [
+    'image/gif',
+    'image/jpeg',
+    'image/png'
+  ];
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileDataUri = '';
+  errorMsg = '';
+
+  previewFile() {
+    const file = this.fileInput.nativeElement.files[0];
+    if (file && this.validateFile(file)) {
+
+      const reader = new FileReader();
+      reader.readAsDataURL(this.fileInput.nativeElement.files[0]);
+      reader.onload = () => {
+        this.fileDataUri = reader.result;
+      }
+    } else {
+      this.errorMsg = 'File must be jpg, png, or gif and cannot be exceed 500 KB in size'
+    }
+  }
+
+  uploadFile(event: Event) {
+    event.preventDefault();
+
+    // get only the base64 file
+    if (this.fileDataUri.length > 0) {
+      const base64File = this.fileDataUri.split(',')[1];
+      // TODO: send to server
+      console.log(base64File);
+    }
+
+  }
+
+  validateFile(file) {
+    return this.acceptedMimeTypes.includes(file.type) && file.size < 500000;
+  }
+
 }
