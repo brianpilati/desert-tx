5e8ba1c33b8255d53ced00341dfff3b5a4acd748
Brian Pilati <brianpilati@gmail.com>
2018-08-14 17:38:30 -0600

    Added an error handled for file-upload

--- node-server/routes/file-upload/index.js
+++ node-server/routes/file-upload/index.js
@@ -10,13 +10,17 @@ router.post('/', cors(corsOptions), function(req, res) {
   fileUploadDomain.fileUpload(req.body, utilities.getToken(req)).then(function() {
     res.status(200);
     res.send();
-  })
+  }).catch(function(_error_) {
+    res.status(401).json(_error_);
+  });
 });
 
 router.get('/', cors(corsOptions), function(req, res) {
   fileUploadDomain.getFiles(utilities.getToken(req)).then(function(_files_) {
     res.status(200).json(_files_);
-  })
+  }).catch(function(_error_) {
+    res.status(401).json(_error_);
+  });
 });
 
 module.exports = router;  
